e225d66132e9c7cd6160a72d9b2318a7740eee26
abelsalim <abelsalim@proton.me>
2023-05-06 06:53:20 -0300

    arquivo de constantes

---
+++ install_packages/constantes.py
@@ -0,0 +1,28 @@
+ATUALIZA = '{} -Sy'
+INSTALA = '{} -S {} --noconfirm'
+UPGRADE = '{} -Syu  --noconfirm'
+LISTA_NAO_INFORMADA = 'É necessário repassar a LISTA DE PACOTES'
+GERENCIADOR_NAO_INFORMADO = 'É necessário informar um GERENCIADOR DE PACOTES'
+GERENCIADOR_NAO_EXECUTADO = (
+    'o gerenciador de pacotes {} não pode ser executado pelo usuário {}.'
+)
+GERENCIADOR_DESCONHECIDO = 'Gerenciador de pacotes {} é desconhecido'
+
+LISTA_VAZIA = 'Lista de pacotes vazia!'
+ERRO_AO_LER_ARQUIVO = 'Não foi possível ler o arquivo com os pacotes.'
+PACOTE_NAO_ENCONTRADO = 'Não foi possível encontrar/instalar o pacote {}'
+
+DESCRICAO = (
+    'Script de instalação em massa para lista de pacotes para o pacman e yay'
+)
+ARGS_GERRENCIADOR = 'Gerenciador de pacotes para utilização'
+ARGS_LISTA = 'Lista com os pacotes instaláveis'
+
+ATUALIZANDO_REPOSITORIOS = 'Atualizando repositórios'
+ATUALIZANDO_PACOTES = 'Atualizando pacotes'
+INSTALANDO_PACOTES = 'Instalando Pacotes'
+
+LOGGER_COLOR = {'INFO': 'green', 'ERROR': 'red'}
+
+INSTALACAO_CONCLUIDA = 'Todos os Pacotes foram INSTALADOS COM SUCESSO!'
+STDERR = '-> No AUR package found for'
